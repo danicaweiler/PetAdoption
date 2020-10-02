4e6885ae06eaf53fbb2d1bb2093538cbabfc000b
Maja <mvrebac6652@conestpgac.on.ca>
2020-10-01 23:11:40 -0400

    Created a working Profile Page

--- PetAdoption/Controller/PetDetailViewController.swift
+++ PetAdoption/Controller/PetDetailViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class PetDetailViewController: UIViewController {
 
+    @IBOutlet var petDetailView: UIView!
+    @IBOutlet weak var petImageView: UIImageView!
+    @IBOutlet weak var petNameLabel: UILabel!
+    @IBOutlet weak var petTraitsLabel: UILabel!
+    @IBOutlet weak var petDescLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let petIconImage = UIImage(named:"dog-icon")
+             petImageView?.image = petIconImage ?? UIImage()
+        petNameLabel.text = "Hi my name is Buddy!"
+        petTraitsLabel.text = "Male | Age:3 | Breed:Pomeranian"
+        petDescLabel.text = "Buddy is very playful and loves to chase his ball. He also loves to cuddle"
+        
+        
     }
     
 

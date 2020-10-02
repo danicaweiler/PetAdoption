2d1653aba5230f26f41bfdb41e5c75a4df3d0ef4
Nathan James Lardizabal <nathanjlardizabal@gmail.com>
2020-10-02 13:36:20 -0400

    working searchView page

--- PetAdoption/Controller/SearchViewController.swift
+++ PetAdoption/Controller/SearchViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  PetAdoption
 //
-//  Created by user182198 on 9/28/20.
+//  Created by user182024 (Nathan) on 9/28/20.
 //  Copyright © 2020 Conestoga. All rights reserved.
 //
 
@@ -11,11 +11,21 @@ import Foundation
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var locationSearchBar: UISearchBar!
+    @IBOutlet weak var petType: UISegmentedControl!
+    @IBOutlet weak var petPreviewView: UIImageView!
+    @IBAction func searchPet(_ sender: Any) {
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: Any) {
+        locationSearchBar.resignFirstResponder()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-   
+        
+        // temporary image holder
+        let petIconImage = UIImage(named:"dog-icon")
+                     petPreviewView?.image = petIconImage ?? UIImage()
     }
-
 }
-
